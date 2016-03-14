ab6b8bec1369a9abf0f9ae3aa9b91e39a3a1f77b
amperas17 <amperas17@gmail.com>
2016-03-15 00:13:27 +0300

    ShowFragment recive and reflect note data from bundle

--- app/src/main/java/com/amperas17/smartnotesapp/NoteItemShowFragment.java
+++ app/src/main/java/com/amperas17/smartnotesapp/NoteItemShowFragment.java
@@ -5,6 +5,9 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,13 +15,23 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 /**
  *
  */
-public class NoteItemShowFragment extends Fragment {
+public class NoteItemShowFragment extends Fragment implements LoaderManager.LoaderCallbacks{
+    final static Integer LOADER_ID = 2;
     final String LOG_TAG = "myLogs";
+
+    TextView mTvTitle,mTvContent,mTvRank;
+    ImageView mIvImage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,23 +39,19 @@ public class NoteItemShowFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_note_item_show, container, false);
         setHasOptionsMenu(true);
 
-        Bundle bundle = getArguments();
-        if (bundle!=null){
-            Log.d(LOG_TAG, "" + getArguments().toString());
-            Uri uri = ContentUris.withAppendedId(NoteDBContract.NoteTable.TABLE_URI,
-                    bundle.getInt(NoteDBContract.NoteTable._ID));
-            Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
-            cursor.moveToFirst();
-            do {
-                int i=0;
-                while (i<cursor.getColumnCount()) {
-                    Log.d(LOG_TAG, "" + cursor.getString(i));
-                    i++;
-                }
-            }  while (cursor.moveToNext());
-        } else {
-            Log.d(LOG_TAG, "null");
+        mTvTitle = (TextView)view.findViewById(R.id.tv_note_show_title);
+        mTvContent = (TextView)view.findViewById(R.id.tv_note_show_content);
+        mTvRank = (TextView)view.findViewById(R.id.tv_note_show_rank);
+
+        mIvImage = (ImageView)view.findViewById(R.id.iv_note_show_image);
+
+
+        Bundle arguments = getArguments();
+        if (arguments!=null) {
+            Log.d(LOG_TAG, "onCreateView" + getArguments().toString());
+            getActivity().getSupportLoaderManager().restartLoader(LOADER_ID, arguments, this);
         }
+
         return view;
     }
 
@@ -66,5 +75,48 @@ public class NoteItemShowFragment extends Fragment {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
+
+        Uri uri = ContentUris.withAppendedId(NoteDBContract.NoteTable.TABLE_URI,
+               args.getInt(NoteDBContract.NoteTable._ID));
+
+        return new CursorLoader(getActivity(),uri, null,null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader loader, Object data) {
+        Cursor cursor = (Cursor) data;
+        cursor.moveToFirst();
+
+        mTvTitle.setText(cursor.getString(cursor
+                .getColumnIndex(NoteDBContract.NoteTable.COLUMN_TITLE)));
+
+        mTvContent.setText(cursor.getString(cursor
+                .getColumnIndex(NoteDBContract.NoteTable.COLUMN_CONTENT)));
+
+        int rank_number = cursor.getInt(cursor
+                .getColumnIndex(NoteDBContract.NoteTable.COLUMN_RANK));
+        mTvRank.setText(NoteDBContract.NoteTable.PRIORITIES[rank_number]);
+
+        String imagePath = cursor.getString(cursor
+                .getColumnIndex(NoteDBContract.NoteTable.COLUMN_IMAGE_PATH));
+        if (imagePath != null){
+            Picasso.with(getActivity())
+                    .load(imagePath)
+                    .placeholder(R.drawable.ic_simple_note)
+                    .error(R.drawable.ic_simple_note)
+                    .centerInside()
+                    .into(mIvImage);
+        } else {
+            mIvImage.setImageResource(R.drawable.ic_simple_note);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+
     }
 }
